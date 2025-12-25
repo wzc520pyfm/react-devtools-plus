b95ae0d16e8bfc58f0f1c1e158b0b2e0fd1bde2c
wzc520pyfm <1528857653@qq.com>
2025-12-25 22:32:30 +0800

    chore(playground): remove unused Umi runtime configuration file

--- packages/playground/umi/src/app.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * Umi runtime configuration
- *
- * Note: React/ReactDOM are automatically exposed to window by react-devtools-plus
- */
-
-// Export empty object for umi runtime config (can be used for runtime customization)
-export {}
